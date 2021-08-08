1b7b6f94514024d9281c7f9faf6de5be6de63f12
chen <chenziqiang@corp.netease.com>
2021-08-08 09:36:07 +0800

    delete cache files

--- run.sh
+++ run.sh
@@ -1,7 +1,14 @@
 
-
+echo "Start Training..."
 # train model with sample data or full data
 python -u tools/trainer.py -m models/rank/dlrm/"$1".yaml
 
+rm -rf tools/utils/__pycache__ __pycache__
+
+echo "Start Testing..."
 ## infer model with sample data or full data
 python -u tools/infer.py -m models/rank/dlrm/"$1".yaml
+
+rm -rf tools/utils/__pycache__ __pycache__
+
+echo " Done~"
