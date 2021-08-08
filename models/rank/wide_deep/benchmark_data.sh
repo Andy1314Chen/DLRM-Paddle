94a7371384597448f67589ae343aa05f24a3be57
chen <chenziqiang@corp.netease.com>
2021-08-08 09:17:19 +0800

    delete files

--- models/rank/wide_deep/benchmark_data.sh
+++
@@ -1,5 +0,0 @@
-echo "Begin DownLoad Criteo Data"
-wget --no-check-certificate https://paddlerec.bj.bcebos.com/benchmark/criteo_benchmark_data.tar.gz
-echo "Begin Unzip Criteo Data"
-tar -xf criteo_benchmark_data.tar.gz
-echo "Get Criteo Data Success"
