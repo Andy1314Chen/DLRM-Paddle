d2cd735bec20ee6a7307afa965749562ea32a3b7
chen <chenziqiang@corp.netease.com>
2021-08-08 09:06:14 +0800

    delete files

--- models/recall/word2vec/benchmark/benchmark_data.sh
+++
@@ -1,5 +0,0 @@
-echo "Begin DownLoad One Billion Data"
-wget --no-check-certificate https://paddlerec.bj.bcebos.com/benchmark/word2vec_benchmark_data.tar.gz
-echo "Begin Unzip One Billion Data"
-tar -xf word2vec_benchmark_data.tar.gz
-echo "Get One Billion Data Success"
